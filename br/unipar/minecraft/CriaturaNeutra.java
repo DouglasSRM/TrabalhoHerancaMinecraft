e78a5511a403944199d16221554da01ee44c28b4
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-04-29 20:40:04 -0300

    Create CriaturaNeutra.java

---
+++ br/unipar/minecraft/CriaturaNeutra.java
@@ -0,0 +1,22 @@
+package br.unipar.minecraft;
+
+public class CriaturaNeutra extends Criaturas{
+    private String reacao;
+    private int dano;
+
+    public String getReacao() {
+        return reacao;
+    }
+
+    public void setReacao(String reacao) {
+        this.reacao = reacao;
+    }
+
+    public int getDano() {
+        return dano;
+    }
+
+    public void setDano(int dano) {
+        this.dano = dano;
+    }
+}
