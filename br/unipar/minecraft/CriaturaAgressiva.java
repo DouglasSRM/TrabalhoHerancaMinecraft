ca0a45cd60ff235ab50129764f86efa9a7c67259
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-04-29 20:40:29 -0300

    Create CriaturaAgressiva.java

---
+++ br/unipar/minecraft/CriaturaAgressiva.java
@@ -0,0 +1,22 @@
+package br.unipar.minecraft;
+
+public class CriaturaAgressiva {
+    private int dano;
+    private int areaDeIdentificacao;
+
+    public int getDano() {
+        return dano;
+    }
+
+    public void setDano(int dano) {
+        this.dano = dano;
+    }
+
+    public int getAreaDeIdentificacao() {
+        return areaDeIdentificacao;
+    }
+
+    public void setAreaDeIdentificacao(int areaDeIdentificacao) {
+        this.areaDeIdentificacao = areaDeIdentificacao;
+    }
+}
